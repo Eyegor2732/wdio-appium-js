8ff4388529e2545e5cd4d091b0eb4e6c30ece33a
Igor <igor_kovalsky@yahoo.com>
2026-07-14 17:56:58 -0400

    07-14-2026 1

--- test/specs/android/test.spec.js
+++ test/specs/android/test.spec.js
@@ -1,6 +1,6 @@
 describe('test', () => {
   it('test', async () => {
-    await driver.sendSms('4047935062', 'Hello from Appium!');
+    await driver.sendSms('1234567890', 'Hello from Appium!');
     await driver.pause(3000);
   });
 
